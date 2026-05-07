90808d1ddb1de6308591503af1353aef648b4e82
Enrique Bruzual <bruzual.kike@gmail.com>
2026-05-07 19:36:22 -0400

    chore: remove chroma reproduction script

--- repro_chroma.py
+++
@@ -1,9 +0,0 @@
-import chromadb
-client = chromadb.Client()
-collection = client.create_collection("test")
-collection.upsert(
-    ids=["1"],
-    documents=["test"],
-    metadatas=[{"decorators": []}]
-)
-print("Done")
